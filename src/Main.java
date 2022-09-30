4a6de2848ad93045ce16ca350973c306b276a7ad
Wlady <vovawestland@gmail.com>
2022-09-30 13:20:45 +0200

    Neue Feature wurde erstellt

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@ public class Main {
         System.out.println("Version 1.0");
         System.out.println("Version 2.0");
     }
+
+    public static void newFeature(){
+        System.out.println("Feature is ready");
+    }
 }
